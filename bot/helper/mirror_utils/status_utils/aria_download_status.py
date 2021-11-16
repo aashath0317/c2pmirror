43cf88d5787cfa720c19f3cca0204a9a03afd359
aashath0317 <89287795+aashath0317@users.noreply.github.com>
2021-11-16 04:00:57 -0800

    Update aria_download_status.py

--- bot/helper/mirror_utils/status_utils/aria_download_status.py
+++ bot/helper/mirror_utils/status_utils/aria_download_status.py
@@ -59,11 +59,12 @@ class AriaDownloadStatus(Status):
     def status(self):
         download = self.aria_download()
         if download.is_waiting:
-            return MirrorStatus.STATUS_WAITING
+            status = MirrorStatus.STATUS_WAITING
         elif download.has_failed:
-            return MirrorStatus.STATUS_FAILED
+            status = MirrorStatus.STATUS_FAILED
         else:
-            return MirrorStatus.STATUS_DOWNLOADING
+            status = MirrorStatus.STATUS_DOWNLOADING
+        return status
 
     def aria_download(self):
         self.__update()
@@ -86,14 +87,11 @@ class AriaDownloadStatus(Status):
         LOGGER.info(f"Cancelling Download: {self.name()}")
         download = self.aria_download()
         if download.is_waiting:
-            self.__listener.onDownloadError("Cancelled by user")
+            self.__listener.onDownloadError("★ 𝗗𝗼𝘄𝗻𝗹𝗼𝗮𝗱 𝗖𝗮𝗻𝗰𝗲𝗹𝗹𝗲𝗱 𝗕𝘆 𝗨𝘀𝗲𝗿!! ★")
             aria2.remove([download], force=True)
             return
         if len(download.followed_by_ids) != 0:
             downloads = aria2.get_downloads(download.followed_by_ids)
-            self.__listener.onDownloadError('Download stopped by user!')
             aria2.remove(downloads, force=True)
-            aria2.remove([download], force=True)
-            return
-        self.__listener.onDownloadError('Download stopped by user!')
+        self.__listener.onDownloadError('★ 𝗗𝗼𝘄𝗻𝗹𝗼𝗮𝗱 𝗖𝗮𝗻𝗰𝗲𝗹𝗹𝗲𝗱 𝗕𝘆 𝗨𝘀𝗲𝗿!! ★')
         aria2.remove([download], force=True)
